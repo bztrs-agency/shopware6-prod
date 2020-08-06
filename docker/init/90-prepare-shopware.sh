63f47a885d17da5ffe5f20b3f34900920f2f552c
tijsvanerp <tijs.vanerp@bztrs.nl>
2020-08-06 18:35:06 +0200

    release setup
    Signed-off-by: tijsvanerp <tijs.vanerp@bztrs.nl>

--- docker/init/90-prepare-shopware.sh
+++ docker/init/90-prepare-shopware.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/with-contenv bash
 if [ "$APP_ENV" = "prod" ] ; then
+  touch /www/install.lock
   sudo -E -u www-data ./bin/console database:migrate --all --env="${APP_ENV}"
   sudo -E -u www-data ./bin/console bundle:dump --env="${APP_ENV}"
   sudo -E -u www-data ./bin/console theme:compile --env="${APP_ENV}"
