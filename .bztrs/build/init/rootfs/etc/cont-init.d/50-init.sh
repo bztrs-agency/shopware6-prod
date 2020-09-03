09dcc249e0ea77844aa2de2479ea26ed57348fca
tijsvanerp <tijs.vanerp@bztrs.nl>
2020-09-03 10:28:34 +0200

    moved theme:compile to web-app container instead of init
    Signed-off-by: tijsvanerp <tijs.vanerp@bztrs.nl>

--- .bztrs/build/init/rootfs/etc/cont-init.d/50-init.sh
+++ .bztrs/build/init/rootfs/etc/cont-init.d/50-init.sh
@@ -2,8 +2,4 @@
 if [ "$APP_ENV" = "prod" ] ; then
   touch /www/install.lock
   /www/console database:migrate --all --env="${APP_ENV}"
-  /www/console bundle:dump --env="${APP_ENV}"
-  /www/console theme:dump --env="${APP_ENV}"
-  /www/console theme:compile --env="${APP_ENV}"
-  /www/console assets:install --env="${APP_ENV}"
 fi
--- .bztrs/build/web-app/rootfs/etc/cont-init.d/90-prepare-shopware.sh
+++ .bztrs/build/web-app/rootfs/etc/cont-init.d/90-prepare-shopware.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/with-contenv bash
 if [ "$APP_ENV" = "prod" ] ; then
   touch /www/install.lock
+  /www/console bundle:dump --env="${APP_ENV}"
+  /www/console theme:dump --env="${APP_ENV}"
+  /www/console theme:compile --env="${APP_ENV}"
+  /www/console assets:install --env="${APP_ENV}"
   /www/console cache:warmup --env="${APP_ENV}"
   /www/console http:cache:warm:up --env="${APP_ENV}"
 fi
