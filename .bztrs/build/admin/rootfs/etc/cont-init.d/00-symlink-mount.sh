ef442137bef349007c07944ffa8e77b31f4a8905
tijsvanerp <tijs.vanerp@bztrs.nl>
2020-10-06 10:06:43 +0200

    added opcache
    Signed-off-by: tijsvanerp <tijs.vanerp@bztrs.nl>

--- .bztrs/build/admin/rootfs/etc/cont-init.d/00-symlink-mount.sh
+++ .bztrs/build/admin/rootfs/etc/cont-init.d/00-symlink-mount.sh
@@ -2,13 +2,14 @@
 if [[ "$APP_ENV" = "prod" ]] ; then
   mkdir -p /mnt/nfs/custom/plugins
   mkdir -p /mnt/nfs/config/jwt
-  mkdir -p /mnt/nfs/files/{private,public}
+  mkdir -p /mnt/nfs/files/{private,public,import}
   rm -r /www/config/jwt
   rm -fr /www/public/recovery
   rm -r /www/custom/plugins
   chmod +x /www/console
   sudo -u www-data ln -sfn /mnt/nfs/custom/plugins /www/custom/plugins
   sudo -u www-data ln -sfn /mnt/nfs/config/jwt /www/config/jwt
+  sudo -u www-data ln -sfn /mnt/nfs/files /www/public/files
 fi
 
 chown -R www-data:www-data /www/var/cache
