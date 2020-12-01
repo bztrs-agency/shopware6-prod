8e5f19a847fafa4051ff1487c7a27540d81022a3
tijsvanerp <tijs@vanerp.io>
2020-12-01 13:40:57 +0100

    removed removal of recovery

--- .bztrs/build/admin/rootfs/etc/cont-init.d/00-symlink-mount.sh
+++ .bztrs/build/admin/rootfs/etc/cont-init.d/00-symlink-mount.sh
@@ -4,7 +4,6 @@ if [[ "$APP_ENV" = "prod" ]] ; then
   mkdir -p /mnt/nfs/config/jwt
   mkdir -p /mnt/nfs/files/{private,public,import}
   rm -r /www/config/jwt
-  rm -fr /www/public/recovery
   rm -r /www/custom/plugins
   chmod +x /www/console
   sudo -u www-data ln -sfn /mnt/nfs/custom/plugins /www/custom/plugins
